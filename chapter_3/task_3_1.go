2a9bd66297e3886ed2b7b6840dd5b45237f7e3c8
Robin Maasjosthusmann <robin@maasjosthusmann.de>
2022-04-25 14:32:29 +0200

    Skip invalid polygons

--- chapter_3/task_3_1.go
+++ chapter_3/task_3_1.go
@@ -26,33 +26,42 @@ func main() {
 
 	for i := 0; i < cells; i++ {
 		for j := 0; j < cells; j++ {
+			ax, ay, aOk := corner(i+1, j)
+			bx, by, bOk := corner(i, j)
+			cx, cy, cOk := corner(i, j+1)
+			dx, dy, dOk := corner(i+1, j+1)
 
-			ax, ay := corner(i+1, j)
-			bx, by := corner(i, j)
-			cx, cy := corner(i, j+1)
-			dx, dy := corner(i+1, j+1)
-			fmt.Printf("<polygon points='%f,%g %g,%g %g,%g %g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
+			// If no error occures, print polygon
+			if aOk && bOk && cOk && dOk {
+				fmt.Printf("<polygon points='%f,%g %g,%g %g,%g %g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
+			}
 		}
 	}
 	fmt.Println("</svg>")
 }
 
-func corner(i, j int) (float64, float64) {
+func corner(i, j int) (float64, float64, bool) {
 	// Find point (x,y) at corner of cell (i, j)
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
 	// Compute surface height z
-	z := f(x, y)
+	z, ok := f(x, y)
+	if !ok {
+		return 0.0, 0.0, false
+	}
 
 	// Project (x,y,z) isometrically onto 2D SVG canvas (sx, sy)
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := width/2 + (x+y)*sin30*xyscale - z*zscale
-	return sx, sy
+	return sx, sy, true
 }
 
-func f(x, y float64) float64 {
+func f(x, y float64) (float64, bool) {
 	r := math.Hypot(x, y) // distance from (0,0)
-	return math.Sin(r) / r
-
+	surfaceHeight := math.Sin(r) / r
+	if math.IsNaN(surfaceHeight) {
+		return 0.0, false
+	}
+	return surfaceHeight, true
 }
