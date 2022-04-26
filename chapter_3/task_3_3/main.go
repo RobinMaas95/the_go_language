488b205c69787085ec673bdcf86cbdebc753a1e9
Robin Maasjosthusmann <robin@maasjosthusmann.de>
2022-04-26 16:14:39 +0200

    Add height depending coloring

--- chapter_3/task_3_3/main.go
+++ chapter_3/task_3_3/main.go
@@ -56,7 +56,8 @@ func main() {
 }
 
 func svg(w io.Writer, targetMathFunction mathFunction) {
-	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+"style='stroke: blue; fill: whithe; stroke-width: 0.7' "+
+	zmin, zmax := minmax(targetMathFunction)
+	fmt.Fprintf(w, "<svg xmlns='http://www.w3.org/2000/svg' "+"style='stroke: grey; fill: whithe; stroke-width: 0.7' "+
 		"width='%d' height='%d'>", width, height)
 
 	for i := 0; i < cells; i++ {
@@ -68,7 +69,7 @@ func svg(w io.Writer, targetMathFunction mathFunction) {
 
 			// If no error occures, print polygon
 			if aOk && bOk && cOk && dOk {
-				fmt.Fprintf(w, "<polygon points='%f,%g %g,%g %g,%g %g,%g'/>\n", ax, ay, bx, by, cx, cy, dx, dy)
+				fmt.Fprintf(w, "<polygon points='%f,%g %g,%g %g,%g %g,%g' style='stroke:%s'/>\n", ax, ay, bx, by, cx, cy, dx, dy, color(i, j, zmin, zmax, targetMathFunction))
 			}
 		}
 	}
@@ -92,6 +93,62 @@ func corner(i, j int, targetMathFunction mathFunction) (float64, float64, bool) 
 	return sx, sy, true
 }
 
+func minmax(f mathFunction) (min float64, max float64) {
+	min = math.NaN()
+	max = math.NaN()
+	for i := 0; i < cells; i++ {
+		for j := 0; j < cells; j++ {
+			for xoff := 0; xoff <= 1; xoff++ {
+				for yoff := 0; yoff <= 1; yoff++ {
+					x := xyrange * (float64(i+xoff)/cells - 0.5)
+					y := xyrange * (float64(j+yoff)/cells - 0.5)
+					z, _ := f(x, y)
+					if math.IsNaN(min) || z < min {
+						min = z
+					}
+					if math.IsNaN(max) || z > max {
+						max = z
+					}
+				}
+			}
+		}
+	}
+	return
+}
+
+func color(i, j int, zmin, zmax float64, f mathFunction) string {
+	min := math.NaN()
+	max := math.NaN()
+	for xoff := 0; xoff <= 1; xoff++ {
+		for yoff := 0; yoff <= 1; yoff++ {
+			x := xyrange * (float64(i+xoff)/cells - 0.5)
+			y := xyrange * (float64(j+yoff)/cells - 0.5)
+			z, _ := f(x, y)
+			if math.IsNaN(min) || z < min {
+				min = z
+			}
+			if math.IsNaN(max) || z > max {
+				max = z
+			}
+		}
+	}
+
+	color := ""
+	if math.Abs(max) > math.Abs(min) {
+		red := math.Exp(math.Abs(max)) / math.Exp(math.Abs(zmax)) * 255
+		if red > 255 {
+			red = 255
+		}
+		color = fmt.Sprintf("#%02x0000", int(red))
+	} else {
+		blue := math.Exp(math.Abs(min)) / math.Exp(math.Abs(zmin)) * 255
+		if blue > 255 {
+			blue = 255
+		}
+		color = fmt.Sprintf("#0000%02x", int(blue))
+	}
+	return color
+}
 func org(x, y float64) (float64, bool) {
 	r := math.Hypot(x, y) // distance from (0,0)
 	surfaceHeight := math.Sin(r) / r
